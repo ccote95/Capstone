422c3ee0b2a54b2345a0ba549cda60147cb98828
Chad Cote <chadcote58@gmail.com>
2024-03-12 15:27:14 -0400

    Wrote logic for submitting a new comment

--- src/components/services/postService.js
+++ src/components/services/postService.js
@@ -17,3 +17,14 @@ export const getAllFormats = () => {
 export const getAllComments = () => {
   return fetch(`http://localhost:8000/comments`).then((res) => res.json());
 };
+
+export const submitNewComment = (newComment) => {
+  const postOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newComment),
+  };
+  return fetch("http://localhost:8000/comments", postOptions);
+};
