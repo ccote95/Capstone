477195426c7fc689abec9b7737804e7fc4887998
Chad Cote <chadcote58@gmail.com>
2024-03-12 09:01:21 -0400

    wrote logic for rerendering the page and implimented that in a useEffect

--- src/components/posts/PostDetails.jsx
+++ src/components/posts/PostDetails.jsx
@@ -1,7 +1,24 @@
 import { useEffect, useState } from "react";
+import { getPostById } from "../services/postService.js";
 
 export const PostDetails = () => {
   const [post, setPost] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    reRenderPage();
+  }, []);
+
+  const reRenderPage = () => {
+    getPostById(postId).then((post) => {
+      setPost(post);
+    });
+  };
+
+  return (
+    <article>
+      <section>
+        <div>{post.title}</div>
+      </section>
+    </article>
+  );
 };
