f8ac0b1486f1d537ba80e32e0341949c75242879
Chad Cote <chadcote58@gmail.com>
2024-03-12 08:52:17 -0400

    Created the post details component

--- src/components/posts/AllPosts.jsx
+++ src/components/posts/AllPosts.jsx
@@ -1,7 +1,11 @@
-/**this is to render all of the posts in the database. a user should only see a posts title and the format. but when they click the title of a post it should bring you to the post details view
- * im going to need to get all posts and store them in state.
- * i will need to map the posts
- * create another module for the layout of a post
+/**
+ * create the new post view and add the route to application views
+ * write the logic for submitting a new post
+ * Create the post details view
+ * show the post title, format, deck, body, date, comments
+ * figure out how to implement a comment section in post details
+ * make it so that when i click on a title that i am brought to the posts details view
+ 
  */
 
 import { useEffect, useState } from "react";
---
+++ src/components/posts/PostDetails.jsx
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+
+export const PostDetails = () => {
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {}, []);
+};
