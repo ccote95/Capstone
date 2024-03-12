0e96da68288dbbffb48dee4eac7040a656470601
Chad Cote <chadcote58@gmail.com>
2024-03-12 09:17:54 -0400

    wrapped a post title in a link so when you click on the title you are brought to the post details page

--- src/components/posts/PostDetails.jsx
+++ src/components/posts/PostDetails.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { getPostById } from "../services/postService.js";
+import { useParams } from "react-router";
 
 export const PostDetails = () => {
   const [post, setPost] = useState([]);
+  const { postId } = useParams();
 
   useEffect(() => {
     reRenderPage();
--- src/components/posts/PostLayout.jsx
+++ src/components/posts/PostLayout.jsx
@@ -1,10 +1,18 @@
+import { Link } from "react-router-dom";
 import "./post.css";
 
 export const PostLayout = ({ post }) => {
   return (
     <div className="post">
       <div className="post-title">
-        <h3>{post.title}</h3>
+        <h3>
+          <Link
+            to={`/allposts/${post.id}`}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            {post.title}
+          </Link>
+        </h3>
       </div>
       <div className="post-format">{post.format.name}</div>
       <div className="post-date">{post.date}</div>
