5b4159707946e497c696d36fe6bd96d19c2825b3
Chad Cote <chadcote58@gmail.com>
2024-03-12 08:58:12 -0400

    Wrote logic for getting a post by its Id

--- src/components/services/postService.js
+++ src/components/services/postService.js
@@ -4,6 +4,12 @@ export const getAllPosts = () => {
   );
 };
 
+export const getPostById = (postId) => {
+  return fetch(
+    ` http://localhost:8000/posts/${postId}?_embed=comments&_expand=format&_expand=deck`
+  ).then((res) => res.json());
+};
+
 export const getAllFormats = () => {
   return fetch("http://localhost:8000/formats").then((res) => res.json());
 };
