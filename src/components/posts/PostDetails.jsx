31b1452758ca5fa64d495b88ee309acbc6af97c7
Chad Cote <chadcote58@gmail.com>
2024-03-12 12:33:50 -0400

    Rendered the comments section on post details

--- src/components/posts/PostDetails.jsx
+++ src/components/posts/PostDetails.jsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router";
 export const PostDetails = ({ currentUser }) => {
   const [post, setPost] = useState([]);
   const { postId } = useParams();
+  const [comment, setComment] = useState("");
 
   useEffect(() => {
     reRenderPage();
@@ -16,27 +17,41 @@ export const PostDetails = ({ currentUser }) => {
     });
   };
 
+  const onChangeHandler = (e) => {
+    setComment(e.target.value);
+  };
+
   return (
-    <article className="post-details">
-      <section className="post-info">
-        <div className="post-title">{post.title}</div>
-        <div className="post-deck">{post.deck?.name}</div>
-        <div className="post-format">{post.format?.name}</div>
-        <div className="post-author">{post.user?.fullName}</div>
-        <div className="post-info-date">{post.date}</div>
-      </section>
-      <section className="post-details-body">
-        <div>{post.body}</div>
-      </section>
-      <section>
-        {currentUser?.id === post.userId ? (
-          <div className="edit-post">
-            <button className="edit-post-btn">Edit Post</button>
-          </div>
-        ) : (
-          ""
-        )}
-      </section>
-    </article>
+    <div>
+      <article className="post-details">
+        <section className="post-info">
+          <div className="post-title">{post.title}</div>
+          <div className="post-deck">{post.deck?.name}</div>
+          <div className="post-format">{post.format?.name}</div>
+          <div className="post-author">{post.user?.fullName}</div>
+          <div className="post-info-date">{post.date}</div>
+        </section>
+        <section className="post-details-body">
+          <div>{post.body}</div>
+        </section>
+        <section>
+          {currentUser?.id === post.userId ? (
+            <div className="edit-post">
+              <button className="edit-post-btn">Edit Post</button>
+            </div>
+          ) : (
+            ""
+          )}
+        </section>
+      </article>
+      <div className="main-comment-container">
+        <div className="comment-container">{comment}</div>
+        <div className="comment-flexbox">
+          <h3>Comment</h3>
+          <textarea className="input-box" onChange={onChangeHandler} />
+          <button className="comment-btn">Submit</button>
+        </div>
+      </div>
+    </div>
   );
 };
