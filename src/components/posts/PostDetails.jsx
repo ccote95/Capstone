96c76790a2b50cd847cb9dc289277398e2a9af56
Chad Cote <chadcote58@gmail.com>
2024-03-12 11:23:23 -0400

    added some styling to the post details

--- src/components/posts/PostDetails.jsx
+++ src/components/posts/PostDetails.jsx
@@ -17,19 +17,25 @@ export const PostDetails = ({ currentUser }) => {
   };
 
   return (
-    <article>
+    <article className="post-details">
       <section className="post-info">
-        <div>{post.title}</div>
-        <div>{post.deck?.name}</div>
-        <div>{post.format?.name}</div>
-        <div>{post.user?.fullName}</div>
+        <div className="post-title">{post.title}</div>
+        <div className="post-deck">{post.deck?.name}</div>
+        <div className="post-format">{post.format?.name}</div>
+        <div className="post-author">{post.user?.fullName}</div>
+        <div className="post-info-date">{post.date}</div>
       </section>
-      <section>
+      <section className="post-details-body">
         <div>{post.body}</div>
       </section>
       <section>
-        {post.date}
-        {currentUser.id === post.userId ? <button>Edit Post</button> : ""}
+        {currentUser?.id === post.userId ? (
+          <div className="edit-post">
+            <button className="edit-post-btn">Edit Post</button>
+          </div>
+        ) : (
+          ""
+        )}
       </section>
     </article>
   );
