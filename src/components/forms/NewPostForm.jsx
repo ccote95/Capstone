667a6831269b89e8e411ad89c553f783c61f9b3b
Chad Cote <chadcote58@gmail.com>
2024-03-13 10:19:42 -0400

    got the format drop down to render

--- src/components/forms/NewPostForm.jsx
+++ src/components/forms/NewPostForm.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
-import { getPostById } from "../services/postService.js";
+import { getAllFormats, getPostById } from "../services/postService.js";
+import { FormatDropDown } from "../posts/FormatDropDown.jsx";
 
 export const NewPostForm = ({ currentUser }) => {
   const [post, setPost] = useState({
@@ -8,10 +9,10 @@ export const NewPostForm = ({ currentUser }) => {
     formatId: 0,
     deckId: 0,
     body: "",
-    userId: currentUser.id,
     gameOver: false,
     date: "",
   });
+  const [format, setFormat] = useState([]);
   const navigate = useNavigate();
   const { postId } = useParams();
   /**if there is a postId present then get that post by its id,  */
@@ -23,12 +24,27 @@ export const NewPostForm = ({ currentUser }) => {
     }
   }, []);
 
+  useEffect(() => {
+    getAllFormats().then((formatObj) => {
+      setFormat(formatObj);
+    });
+  }, []);
+
   return (
     <form>
       <fieldset>
         <div>
           <label>Title</label>
           <input type="text" placeholder="title of post" />
+        </div>
+        <div>
+          <label>Format</label>
+          <select>
+            <option>Select a Format</option>
+            {format.map((formatObj) => {
+              return <FormatDropDown format={formatObj} />;
+            })}
+          </select>
         </div>
       </fieldset>
     </form>
