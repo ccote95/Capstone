8ca1bc1e7914274cd7a1e74237f7832c587996c4
Chad Cote <chadcote58@gmail.com>
2024-03-14 11:12:41 -0400

    Created a function to add a new deck

--- src/components/services/deckService.js
+++ src/components/services/deckService.js
@@ -1,3 +1,14 @@
 export const getAllDecks = () => {
   return fetch("http://localhost:8000/decks").then((res) => res.json());
 };
+
+export const addADeck = (deck) => {
+  const postOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(deck),
+  };
+  return fetch(" http://localhost:8000/decks", postOptions);
+};
