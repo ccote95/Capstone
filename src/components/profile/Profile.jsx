be50dd24536d0660b67f0af89448b8d8955e28bf
Chad Cote <chadcote58@gmail.com>
2024-03-14 11:32:43 -0400

    Rendered a modal when you click the add deck button

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -3,6 +3,8 @@ import { getUserInfoByCurrentUserId } from "../services/profileService.js";
 
 export const Profile = ({ currentUser }) => {
   const [userProfile, setUserProfile] = useState();
+  const [showModal, setShowModal] = useState(false);
+  const [newDeckName, setNewDeckName] = useState("");
 
   useEffect(() => {
     if (currentUser) {
@@ -11,6 +13,8 @@ export const Profile = ({ currentUser }) => {
       });
     }
   }, [currentUser]);
+
+  const handleAddingNewDeck = () => {};
 
   return (
     <div className="profile-container">
@@ -24,12 +28,31 @@ export const Profile = ({ currentUser }) => {
         <label>Decks</label>
         <div className="user-decks">
           {userProfile?.deck.name}
-          <button className="add-deck-btn">Add A Deck</button>
+          <button className="add-deck-btn" onClick={() => setShowModal(true)}>
+            Add A Deck
+          </button>
         </div>
         <div className="edit-profile-btn-container">
           <button className="edit-profile-btn">Edit Profile</button>
         </div>
       </div>
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={() => setShowModal(false)}>
+              &times;
+            </span>
+            <h2>Add New Deck</h2>
+            <input
+              type="text"
+              value={newDeckName}
+              onChange={(e) => setNewDeckName(e.target.value)}
+              placeholder="Enter deck name"
+            />
+            <button onClick={handleAddingNewDeck}>Add</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
