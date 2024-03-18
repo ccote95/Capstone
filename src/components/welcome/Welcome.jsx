8e7141ea9d39d4d80789f0b5fdec51e9a1ede1ed
Chad Cote <chadcote58@gmail.com>
2024-03-18 09:50:28 -0400

    Added some styling to the edit profile page

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -2,9 +2,12 @@ import "./Welcome.css";
 
 export const Welcome = () => {
   return (
-    <div className="welcome-container">
-      <div className="welcome-title">
-        <h1>Welcome to TBD</h1>
+    <div className="page-background">
+      <div className="welcome-container">
+        <div className="welcome-title">
+          <h1>Welcome to MTGConnect</h1>
+          <p>Finding groups made easy </p>
+        </div>
       </div>
     </div>
   );
