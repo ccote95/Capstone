bbbe7f752658f00db6271eafc3d654a4612dc579
Chad Cote <chadcote58@gmail.com>
2024-03-19 11:12:54 -0400

    finished the css for the profile section

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -36,59 +36,75 @@ export const Profile = ({ currentUser }) => {
   };
 
   return (
-    <div className="profile-container">
-      <div className="user-info">
-        <label className="flex fullname">
-          <h4>Full Name:</h4>
-          <div className="user-fullname">{userProfile?.fullName}</div>
-        </label>
-        <label className="flex age">
-          <h4>Age:</h4>
+    <div className="page-container">
+      <div className="profile-container">
+        <div className="user-info">
+          <label className="flex fullname">
+            <h4>Full Name:</h4>
+            <div className="user-fullname">{userProfile?.fullName}</div>
+          </label>
+          <label className="flex age">
+            <h4>Age: </h4>
 
-          <div className="user-age">{userProfile?.age}</div>
-        </label>
-        <label className="flex">
-          <h4>Email:</h4>
-          <div className="user-email">{userProfile?.email}</div>
-        </label>
-        <label className="deck-label">Decks:</label>
+            <div className="user-age"> {userProfile?.age}</div>
+          </label>
+          <label className="flex email">
+            <h4>Email:</h4>
+            <div className="user-email">{userProfile?.email}</div>
+          </label>
+          <label className="deck-label">Decks:</label>
+        </div>
         <div className="user-decks">
           {userProfile?.decks.map((deck) => {
-            return <li>{deck.name}</li>;
+            return (
+              <li key={deck.id} className="list-item">
+                {deck.name}
+              </li>
+            );
           })}
         </div>
-        <div className="add-deck-btn-container"></div>
-        <div className="edit-profile-btn-container">
-          <button
-            className="edit-profile-btn"
-            onClick={() => {
-              handleEditClick();
-            }}
-          >
-            Edit Profile
-          </button>
-          <button className="add-deck-btn" onClick={() => setShowModal(true)}>
-            Add A Deck
-          </button>
-        </div>
-      </div>
-      {showModal && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={() => setShowModal(false)}>
-              &times;
-            </span>
-            <h2>Add New Deck</h2>
-            <input
-              type="text"
-              value={newDeckName}
-              onChange={(e) => setNewDeckName(e.target.value)}
-              placeholder="Enter deck name"
-            />
-            <button onClick={handleAddingNewDeck}>Add</button>
+
+        <div className="btn-wrapper">
+          <div className="edit-profile-btn-container">
+            <div className="btn-container">
+              <button
+                className="edit-profile-btn"
+                onClick={() => {
+                  handleEditClick();
+                }}
+              >
+                Edit Profile
+              </button>
+            </div>
+            <div className="btn-container">
+              <button
+                className="add-deck-btn"
+                onClick={() => setShowModal(true)}
+              >
+                Add A Deck
+              </button>
+            </div>
           </div>
         </div>
-      )}
+
+        {showModal && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={() => setShowModal(false)}>
+                &times;
+              </span>
+              <h2>Add New Deck</h2>
+              <input
+                type="text"
+                value={newDeckName}
+                onChange={(e) => setNewDeckName(e.target.value)}
+                placeholder="Enter deck name"
+              />
+              <button onClick={handleAddingNewDeck}>Add</button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
